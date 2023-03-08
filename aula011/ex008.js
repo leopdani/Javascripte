f88dab6e58f84e38ec06a61856e7881009ed50f2
leopdani <51030652+leopdani@users.noreply.github.com>
2023-03-08 00:08:30 -0300

    2 - atualização javascript

---
+++ aula011/ex008.js
@@ -0,0 +1,6 @@
+var vel = 78.2
+console.log(`a velocidade do carro é ${vel} km/h`)
+if(vel > 60){
+    console.log(`vc ultrapassou a velocidade permitida. mutadoooo`) 
+}
+console.log(`digira sempre usando o cinto de segurança`)
---
+++ aula011/ex009.js
@@ -0,0 +1,7 @@
+var pais = "eua"
+console.log(`vivendo em ${pais}`)
+if (pais == 'brasil'){
+    colsole.log('vc é brasileiro')
+}else {
+    console.log('vc é estrangeiro')
+}
